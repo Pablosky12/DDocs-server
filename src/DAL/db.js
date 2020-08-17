74f46efef2df7174069e838d618e699b8af26855
Stiv <pablo1012@outlook.com>
2020-08-17 19:35:51 -0300

    add connection string

--- src/DAL/db.js
+++ src/DAL/db.js
@@ -1,14 +1,7 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASS,
-  port: process.env.PG_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL
 });
 
 
