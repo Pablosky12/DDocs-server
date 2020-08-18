01b7fb4bc51200c2cb1a0f6afd9eb58864a2ba21
Stiv <pablo1012@outlook.com>
2020-08-17 21:26:09 -0300

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import "dotenv/config";
+
+Sentry.init({ dsn: 'https://2212f7d470844761a77920ea9eb6f89c@o435411.ingest.sentry.io/5394671' });
+
 const express = require("express");
 const mountRoutes = require("./routes");
 
 const app = express();
+app.use(Sentry.Handlers.requestHandler());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mountRoutes(app);
